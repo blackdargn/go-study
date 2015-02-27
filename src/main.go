3f60078ac6ce2dbf9185f744ad3c3863ee9c401b
blackdargn <zhaodh2008@gmail.com>
2015-02-27 17:32:58 +0800

    update

--- src/main.go
+++ src/main.go
@@ -1,14 +1,13 @@
 package main
-
 import "study"
 
 func main() {
 //	study.TestGoroutines()
 //	study.TestCommandLineArguments()
 //	study.TestCommandLineFlag()
-//	study.TestEnvironmentVariables()
+    study.TestEnvironmentVariables()
 //	study.TestSpawningProcesses()
 //	study.TestExecingProcesses()
-	study.TestSignals()
+//	study.TestSignals()
 }
 
